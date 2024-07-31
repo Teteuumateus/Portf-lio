24a88bf54a922e5f004fe0dd3771f10c8795850d
Mateus Andrade <Mateusandrade.brasil@gmail.com>
2024-07-31 15:59:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("a[href^='#']");
+    
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute("href")).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+});
